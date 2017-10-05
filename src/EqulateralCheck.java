9801233b870a2779a54c0fa2df6138c80b358987
GraphicStarr <mail2yonz@gmail.com>
2017-10-05 08:57:40 -0400

    Second commit after making some changes

--- src/EqulateralCheck.java
+++ src/EqulateralCheck.java
@@ -6,19 +6,20 @@ public class EqulateralCheck {
 
     public static void main(String args[])
     {
-        System.out.println("Please Enter the first side of the triangele");
+        System.out.println("Please Enter the first side of the Triangele");
         Scanner keyboard= new Scanner(System.in);
         int a= keyboard.nextInt();
-        System.out.println("Please Enter the second side of the triangele");
+        System.out.println("Please Enter the second side of the Triangele");
         keyboard= new Scanner(System.in);
         int b= keyboard.nextInt();
 
-        System.out.println("Please Enter the third side of the triangele");
+        System.out.println("Please Enter the third side of the Triangele");
          keyboard= new Scanner(System.in);
         int c = keyboard.nextInt();
 
        isEqulateral( a,b, c);
     }
+    //Shows us if the Triangle is Equilateral triangle or not
     private static void  isEqulateral(int side,int hight,int hypo)
 
     {
@@ -26,10 +27,11 @@ public class EqulateralCheck {
         double sSqr = Math.pow(side, 2);
         double hSqr = Math.pow(hight, 2);
         double hySqr = Math.pow(hypo, 2);
+
         if (side == hight && hight == hypo) {
-            System.out.println( "IS equlateral");
+            System.out.println( "IS equlateral Triangle");
         }else if (hySqr == (hSqr + sSqr)) {
-            System.out.println("Is Right TriAngle");
+            System.out.println("Is Right Angle TriAngle");
         } else {
             System.out.println("is not Right Angle or equlateral triangle");
         }
